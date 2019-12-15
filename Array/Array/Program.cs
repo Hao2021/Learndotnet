1defabe6a0451195387a12cc7a2e56bc7903801a
Hao2021 <33287196+Hao2021@users.noreply.github.com>
2019-12-15 13:55:37 +0700

    c# basic Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] i = { 1, 2, 3, 4, 5 };
+
+        https://docs.microsoft.com/en-us/dotnet/api/system.array.setvalue?view=netframework-4.8
+
+
+        }
+    }
+}
